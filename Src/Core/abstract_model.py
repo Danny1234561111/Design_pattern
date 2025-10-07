39d90384a78a2504b0a0dde399a59f456720232d
Daniil <shornikovdaniil1111@gmail.com>
2025-10-08 01:38:39 +0800

    add core

--- Src/Core/abstract_model.py
+++ Src/Core/abstract_model.py
@@ -1,37 +1,38 @@
 from abc import ABC
 import uuid
 from Src.Core.validator import validator
+from typing import Optional
 
-"""
-Абстрактный класс для наследования моделей
-Содержит в себе только генерацию уникального кода
-"""
+
 class abstact_model(ABC):
-    __unique_code:str
+    __id: str
+    __name: str  # Имя должно быть инициализировано
 
     def __init__(self) -> None:
         super().__init__()
-        self.__unique_code = uuid.uuid4().hex
+        self.__id = uuid.uuid4().hex
 
-    """
-    Уникальный код
-    """
     @property
-    def unique_code(self) -> str:
-        return self.__unique_code
-    
-    @unique_code.setter
-    def unique_code(self, value: str):
+    def id(self) -> str:
+        return self.__id
+
+    @id.setter
+    def id(self, value: str):
         validator.validate(value, str)
-        self.__unique_code = value.strip()
-    
+        self.__id = value.strip()
 
-    """
-    Перегрузка штатного варианта сравнения
-    """
-    def __eq__(self, value) -> bool:
-        if value is  None: return False
-        if not isinstance(value, abstact_model): return False
+    @property
+    def name(self) -> str:
+        return self.__name
 
-        return self.unique_code == value.unique_code
+    @name.setter
+    def name(self, value: str):
+        validator.validate(value, str)
+        if len(value) > 50:
+            raise ValueError("Имя не должно превышать 50 символов.")
+        self.__name = value.strip()
 
+    def __eq__(self, other) -> bool:
+        if isinstance(other, abstact_model):
+            return self.id == other.id #Используем геттер
+        return False
