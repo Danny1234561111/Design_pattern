31712cecc6612256d65f1b01d17152dc7cb483a9
Daniil <shornikovdaniil1111@gmail.com>
2025-10-10 14:06:22 +0800

    add + fix

--- Src/Models/nomenclature_model.py
+++ Src/Models/nomenclature_model.py
@@ -1,60 +1,51 @@
-from Src.Core.validator import validator
-from Src.Core.abstract_model import abstact_model
-from typing import Optional, TYPE_CHECKING
-from Src.Models.nomenclature_group_model import nomenclature_group_model
+from Src.Core.entity_model import entity_model
+from Src.Models.group_model import group_model
 from Src.Models.range_model import range_model
+from Src.Core.validator import validator
 
-class nomenclature_model(abstact_model):
-    __name: str = ""
-    __full_name: str = ""
-    __group: nomenclature_group_model
-    __unit: range_model
 
-    def __init__(self, name: str, full_name: str, group: nomenclature_group_model, unit: range_model):
-        super().__init__()
-        self.name = name
-        self.full_name = full_name
-        self.group = group
-        self.unit = unit
+"""
+Модель номенклатуры
+"""
+class nomenclature_model(entity_model):
+    __group: group_model = None
+    __range: range_model = None
 
+   
+    """
+    Группа номенклатуры
+    """
     @property
-    def name(self) -> str:
-        return self.__name
-
-    @name.setter
-    def name(self, value: str):
-        validator.validate(value, str)
-        self.__name = value.strip()
-
-    @property
-    def full_name(self) -> str:
-        return self.__full_name
-
-    @full_name.setter
-    def full_name(self, value: str):
-        validator.validate(value, str)
-        if len(value) > 255:
-            raise ValueError("Полное наименование не должно превышать 255 символов.")
-        self.__full_name = value.strip()
-
-    @property
-    def group(self) -> nomenclature_group_model:
+    def group(self) -> group_model:
         return self.__group
 
     @group.setter
-    def group(self, value: nomenclature_group_model):
+    def group(self, value: group_model):
+        validator.validate(value,entity_model )
+        self.__group = value    
 
-        from Src.Models.nomenclature_group_model import nomenclature_group_model
-        validator.validate(value, nomenclature_group_model)
-        self.__group = value
-
+    """
+    Единица измерения
+    """
     @property
-    def unit(self) -> range_model:
-        return self.__unit
-
-    @unit.setter
-    def unit(self, value: range_model):
-
-        from Src.Models.range_model import range_model
+    def range(self) -> range_model:
+        return self.__range
+    
+    @range.setter
+    def range(self, value: range_model):
         validator.validate(value, range_model)
-        self.__unit = value
+        self.__range = value
+
+
+    """
+    Универсальный фабричный метод
+    """
+    def create(name:str, group:group_model, range:range_model):
+        validator.validate(name, str)
+        item = nomenclature_model()
+        item.name = name
+        item.group = group
+        item.range = range
+        return item
+        
+    
--- Src/Models/range_model.py
+++ Src/Models/range_model.py
@@ -1,63 +1,68 @@
-from Src.Core.validator import validator
-from Src.Core.abstract_model import abstact_model
-from typing import Optional
+from Src.Core.entity_model import entity_model
+from Src.Core.validator import validator, argument_exception
 
-class range_model(abstact_model):
-    __name: str = ""
-    __base_unit: Optional["range_model"]
-    __conversion_factor: float = 1.0
+"""
+Модель единицы измерения
+"""
+class range_model(entity_model):
+    __value:int = 1
+    __base:'range_model' = None
 
-    def __init__(self, name: str, conversion_factor: float, base_unit: Optional["range_model"] = None):
-        super().__init__()
-        self.name = name
-        self.conversion_factor = conversion_factor
-        self.base_unit = base_unit
-
+    """
+    Значение коэффициента пересчета
+    """
     @property
-    def name(self) -> str:
-        return self.__name
-
-    @name.setter
-    def name(self, value: str):
-        validator.validate(value, str)
-        self.__name = value.strip()
-
-    @property
-    def base_unit(self) -> Optional["range_model"]:
-        return self.__base_unit
-
-    @base_unit.setter
-    def base_unit(self, value: Optional["range_model"]):
-        if value is not None:
-            validator.validate(value, range_model)
-        self.__base_unit = value
-
-    @property
-    def conversion_factor(self) -> float:
-        return self.__conversion_factor
-
-    @conversion_factor.setter
-    def conversion_factor(self, value: float):
-        validator.validate(value, float)
+    def value(self) -> int:
+        return self.__value
+    
+    @value.setter
+    def value(self, value: int):
+        validator.validate(value, int)
         if value <= 0:
-            raise ValueError("Коэффициент пересчета должен быть больше 0.")
-        self.__conversion_factor = value
+             raise argument_exception("Некорректный аргумент!")
+        self.__value = value
 
-    def to_base_unit(self, value: float) -> float:
-        return value * self.conversion_factor
 
-    def from_base_unit(self, value: float) -> float:
-        return value / self.conversion_factor
+    """
+    Базовая единица измерения
+    """
+    @property
+    def base(self):
+        return self.__base
+    
+    @base.setter
+    def base(self, value):
+        self.__base = value
 
-    def get_conversion_factor_to(self, target_unit: "range_model") -> float:
-        if self == target_unit:
-            return 1.0
-        # If we have a base unit, try to convert to that first
-        if self.base_unit is not None:
-            base_to_target = self.base_unit.get_conversion_factor_to(target_unit)
-            if base_to_target is not None:  # Ensure a valid path exists
-                return self.conversion_factor * base_to_target
-            else:
-                return None # No conversion path exists
-        else:
-            return None # If there's no base unit then can't convert.
+    """
+    Киллограмм
+    """
+    @staticmethod
+    def create_kill():
+        inner_gramm = range_model.create_gramm()
+        return range_model.create(  "киллограмм", inner_gramm)
+
+    """
+    Грамм
+    """
+    @staticmethod
+    def create_gramm():
+        return range_model.create("грамм")
+     
+    """
+    Универсальный метод - фабричный
+    """
+    @staticmethod
+    def create(name:str, value:int, base ):
+        validator.validate(name, str)
+        validator.validate(value, int)
+
+        inner_base = None
+        if not base is None: 
+            validator.validate(base, range_model)
+            inner_base = base
+        item = range_model()
+        item.name = name
+        item.base = inner_base
+        item.value = value
+        return item
