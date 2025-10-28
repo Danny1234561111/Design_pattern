3f24ed62c0ac5fc7fb7058f89516c0ebaab97cd2
Daniil <shornikovdaniil1111@gmail.com>
2025-10-28 18:10:04 +0800

    Update validator.py

--- Src/Core/validator.py
+++ Src/Core/validator.py
@@ -1,12 +1,34 @@
 import os
-from typing import Any
-
+from typing import Any, List
 class argument_exception(Exception):
     pass
 
 class operation_exception(Exception):
     pass
-
+def obj_to_dict(object_: Any) -> dict:
+    
+    # Если объект - словарь
+    if type(object_) is dict:
+        d = dict()
+        for k, v in object_.items():
+            d[k] = obj_to_dict(v)
+        return d
+    # Если итерируемый объект
+    elif type(object_) in [list, tuple]:
+        l = list()
+        for item in object_:
+            l += [obj_to_dict(item)]
+        return l
+    # Если объект - AbstractModel с полем 'name'
+    elif isinstance(object_, AbstractModel):
+        return {prop: obj_to_dict(getattr(object_, prop))
+                for prop in get_properties(object_)}
+    # Если примитивный тип
+    elif type(object_) in [bool, int, float, str] or object_ is None:
+        return object_
+    # Всё остальное
+    else:
+        return str(object_)
 class validator:
 
     @staticmethod
